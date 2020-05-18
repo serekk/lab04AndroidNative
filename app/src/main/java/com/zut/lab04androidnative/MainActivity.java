b50b8e2751298129bccc162cd5109fc8cc326f3a
Karol <karolpoczta@gmail.com>
2020-05-18 23:44:40 +0200

    add animal list

--- app/src/main/java/com/zut/lab04androidnative/MainActivity.java
+++ app/src/main/java/com/zut/lab04androidnative/MainActivity.java
@@ -3,12 +3,29 @@ package com.zut.lab04androidnative;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-//TODO: reszta labolatorium
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values = new String[] {"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik",
+        "Mysz", "Jeż", "Myszojeleń"};
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listView = findViewById(R.id.animalListView);
+        listView.setAdapter(this.adapter);
     }
 }
