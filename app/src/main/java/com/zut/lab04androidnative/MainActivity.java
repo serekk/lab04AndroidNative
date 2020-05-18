9c76e25120d48b5e0c7c1c79fe42b677acc7d1b3
Karol <karolpoczta@gmail.com>
2020-05-18 23:30:55 +0200

    add todo

--- app/src/main/java/com/zut/lab04androidnative/MainActivity.java
+++ app/src/main/java/com/zut/lab04androidnative/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//TODO: reszta labolatorium
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
