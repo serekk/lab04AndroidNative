c8650e4181441f3c542539192e44e50ea4f50b6b
Karol <karolpoczta@gmail.com>
2020-05-19 00:04:35 +0200

    utworzony formularz dodawania wpisu

--- app/src/main/java/com/zut/lab04androidnative/MainActivity.java
+++ app/src/main/java/com/zut/lab04androidnative/MainActivity.java
@@ -2,7 +2,11 @@ package com.zut.lab04androidnative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +31,18 @@ public class MainActivity extends AppCompatActivity {
         this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
         ListView listView = findViewById(R.id.animalListView);
         listView.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem menuItem){
+        Intent intent = new Intent(this, DodajWpis.class);
+        startActivityForResult(intent, 1);
     }
 }
